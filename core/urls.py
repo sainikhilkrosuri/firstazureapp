98c48568754a531f7ec6b2fb65aa31ea17776c79
Sai Nikhil Krosuri <110455764+sainikhilkrosuri@users.noreply.github.com>
2025-09-24 22:57:03 -0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATICFILES_DIR)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
